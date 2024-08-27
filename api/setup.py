ad4e7f154fd23e3d96a3b90ffd88850b16557baf
Jusiiek <jakub.zurawski@outlook.com>
2024-08-27 22:05:49 +0200

    Created setup py for api package, #3

---
+++ api/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+requirements = """
+
+"""
+
+setup(
+    name='ai_chat',
+    version='0.0.1',
+    author='<NAME>',
+    author_email='<EMAIL>',
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.10'
+)
