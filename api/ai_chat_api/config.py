25e0c58868ac7fae9ce7328243dddfab9d813f84
Jusiiek <jakub.zurawski@outlook.com>
2024-09-01 00:34:35 +0200

    Created cassandra connector for api, #4

--- api/ai_chat_api/config.py
+++ api/ai_chat_api/config.py
@@ -5,3 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     HOST = os.environ.get('HOST', '0.0.0.0')
     PORT = os.environ.get('PORT', 8000)
+
+    # cassandra
+    CASSANDRA_USERNAME = os.environ.get('CASSANDRA_USERNAME', 'admin')
+    CASSANDRA_PASSWORD = os.environ.get('CASSANDRA_PASSWORD', 'admin')
