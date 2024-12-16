78abf9f87e70b3f7ae54bc3d1af7dd9a2f3b475f
Jakub Zurawski <jakub.zurawski@limebrains.com>
2024-12-16 20:49:21 +0100

    Added middlewares to the api app, #14

--- api/ai_chat_api/app.py
+++ api/ai_chat_api/app.py
@@ -1,6 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
+from ai_chat_api.middleware import (
+    jwt_middleware,
+    security_headers_middleware
+)
 from ai_chat_api.config import Config
 from ai_chat_api.cassandradb import CassandraConnection
 
@@ -9,8 +14,20 @@ def create_app() -> FastAPI:
     """
     Creates FastAPI application.
     """
-    # TODO add middlewares
     app = FastAPI()
+
+    origins = [Config.WEB_HOST]
+    app.middleware("http")(jwt_middleware)
+    app.middleware("http")(security_headers_middleware)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        expose_headers=["*"],
+    )
 
     return app
 
