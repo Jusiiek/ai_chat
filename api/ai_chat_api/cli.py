8639788bdaacbda20eb4abf307fa9ef0d8dd82c1
Jakub Zurawski <jakub.zurawski@limebrains.com>
2024-12-17 22:35:19 +0100

    Updated cli, added init db function, #13

--- api/ai_chat_api/cli.py
+++ api/ai_chat_api/cli.py
@@ -2,6 +2,11 @@ import click
 import asyncio
 from functools import wraps
 
+from ai_chat_api.utils.db import (
+    drop_db,
+    create_db
+)
+
 
 def coroutine(f):
     """takes an asynchronous function f as input."""
@@ -10,3 +15,19 @@ def coroutine(f):
         return asyncio.get_event_loop().run_until_complete(f(*args, **kwargs))
 
     return wrapper
+
+
+@click.group()
+def cli():
+    """This is a management script for ai_chat_api."""
+
+
+@cli.command()
+@coroutine
+async def init_db():
+    await drop_db()
+    await create_db()
+
+
+if __name__ == "__main__":
+    cli()
