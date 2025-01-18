5c56aacba1f430d11a0b4bc809fea6740c0de7b4
Jusiiek <jakub.zurawski@outlook.com>
2025-01-18 12:12:41 +0100

    Updated auth endpoint, #15

--- api/ai_chat_api/api/router/auth.py
+++ api/ai_chat_api/api/router/auth.py
@@ -1,1 +1,35 @@
+from typing import Dict, Any
+
 from fastapi import APIRouter
+
+from ai_chat_api.api.authentication.authenticator import Authenticator
+from ai_chat_api.api.authentication.authentication_backend import AuthenticationBackend
+from ai_chat_api.api.manager import UserManager
+from ai_chat_api.api.models.user import User
+from ai_chat_api.api.protocols import models
+
+
+def get_auth_router(
+    backend: AuthenticationBackend[User, models.ID],
+    user_manager: UserManager[User, models.ID],
+    authenticator: Authenticator[User, models.ID],
+    requires_verification: bool = False,
+) -> APIRouter:
+    """
+    Creates a router with auth routes for an authentication backend.
+    """
+
+    router = APIRouter()
+    get_current_user_token = authenticator.current_user_token(
+        is_active=True, is_verified=requires_verification
+    )
+
+    login_response: Dict[str, Any] = {
+        **backend.bearer_transport.get_success_login_response()
+    }
+
+    logout_response: Dict[str, Any] = {
+        **backend.bearer_transport.get_success_logout_response()
+    }
+
+    return router
