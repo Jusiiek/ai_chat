5874ed71655d74a27c51d54b74c509f1d9307703
Jusiiek <jakub.zurawski@outlook.com>
2025-04-01 12:58:25 +0200

    Created thread and chat models, #48

---
+++ api/ai_chat_api/api/models/chat.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import Union, List
+
+from cassandra.cqlengine import columns
+
+from ai_chat_api.api.models.base import BaseModel
+from ai_chat_api.api.protocols import models
+
+
+class Chat(BaseModel):
+    __tablename__ = 'chat'
+
+    thread_id = columns.UUID(index=True)
+    user_id = columns.UUID(index=True)
+    created_at = columns.DateTime(default=datetime.now())
+    updated_at = columns.DateTime(default=datetime.now())
+    user_message: columns.Text = columns.Text()
+    ai_message: columns.Text = columns.Text()
+
+    @classmethod
+    async def get_by_user_id(cls, user_id: models.ID) -> Union[List["Chat"], List]:
+        return cls.objects.filter(user_id=user_id).allow_filtering()
+
+    @classmethod
+    async def get_by_thread_id(cls, thread_id: models.ID) -> Union[List["Chat"], List]:
+        return cls.objects.filter(thread_id=thread_id).allow_filtering()
---
+++ api/ai_chat_api/api/models/thread.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from typing import List, Union
+
+from cassandra.cqlengine import columns
+
+from ai_chat_api.api.protocols import models
+from ai_chat_api.api.models.base import BaseModel
+from ai_chat_api.api.models.chat import Chat
+
+
+class Thread(BaseModel):
+    __tablename__ = 'thread'
+
+    user_id = columns.UUID(index=True)
+    title = columns.Text()
+    created_at = columns.DateTime(default=datetime.now())
+    updated_at = columns.DateTime(default=datetime.now())
+
+    @property
+    async def conversations(self) -> Union[List[Chat], List]:
+        """
+        Fetch conversations dynamically using thread_id.
+        """
+        return await Chat.get_by_thread_id(thread_id=self.id)
+
+    @classmethod
+    async def get_by_user_id(cls, user_id: models.ID) -> Union[List["Thread"], List]:
+        return cls.objects.filter(user_id=user_id).allow_filtering()
